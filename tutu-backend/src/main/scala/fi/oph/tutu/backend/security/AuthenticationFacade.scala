6fba377549cdc913f0dd953b6d86a0cfada11c56
Tatu Honkanen <tatu.honkanen@rebase.fi>
2025-04-11 08:48:07 +0300

    OK-934: Lisätty get & post HakemuspalveluServiceen

---
+++ tutu-backend/src/main/scala/fi/oph/tutu/backend/security/AuthenticationFacade.scala
@@ -0,0 +1,18 @@
+package fi.oph.tutu.backend.security
+
+import org.springframework.security.core.Authentication
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.stereotype.Component
+
+trait IAuthenticationFacade {
+  def getAuthentication: Authentication
+}
+
+@Component
+class AuthenticationFacade extends IAuthenticationFacade {
+
+  @Override
+  def getAuthentication: Authentication = {
+    SecurityContextHolder.getContext.getAuthentication
+  }
+}
--- tutu-backend/src/main/scala/fi/oph/tutu/backend/service/HakemuspalveluService.scala
+++ tutu-backend/src/main/scala/fi/oph/tutu/backend/service/HakemuspalveluService.scala
@@ -1,9 +1,16 @@
 package fi.oph.tutu.backend.service
 
 import fi.vm.sade.javautils.nio.cas.CasClient
+import org.asynchttpclient.RequestBuilder
 import org.slf4j.{Logger, LoggerFactory}
 import org.springframework.beans.factory.annotation.{Autowired, Value}
 import org.springframework.stereotype.Component
+
+import java.util.concurrent.TimeUnit
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Await
+import scala.concurrent.duration.Duration
+import scala.jdk.javaapi.FutureConverters.asScala
 
 @Component
 class HakemuspalveluService {
@@ -14,4 +21,52 @@ class HakemuspalveluService {
 
   @Autowired
   private val client: CasClient = null
+
+  private def get(url: String): Either[Throwable, String] = {
+    val req = new RequestBuilder()
+      .setMethod("GET")
+      .setUrl(url)
+      .build()
+    try {
+      val result = asScala(client.execute(req)).map {
+        case r if r.getStatusCode == 200 =>
+          Right(r.getResponseBody())
+        case r =>
+          LOG.error(
+            s"Hakemuspalvelu GET failed with: ${r.getStatusCode} ${r.getStatusText} ${r.getResponseBody()}"
+          )
+          Left(new RuntimeException("Hakemuspalvelu GET failed: " + r.getResponseBody()))
+      }
+
+      Await.result(result, Duration(10, TimeUnit.SECONDS))
+    } catch {
+      case e: Throwable =>
+        Left(e)
+    }
+  }
+
+  private def post(url: String, body: String): Either[Throwable, String] = {
+    val req = new RequestBuilder()
+      .setMethod("POST")
+      .setUrl(url)
+      .setBody(body)
+      .build()
+    try {
+      val result = asScala(client.execute(req)).map {
+        case r if r.getStatusCode == 200 =>
+          Right(r.getResponseBody())
+        case r =>
+          LOG.error(
+            s"Hakemuspalvelu POST failed with: ${r.getStatusCode} ${r.getStatusText} ${r.getResponseBody()}"
+          )
+          Left(new RuntimeException("Hakemuspalvelu POST failed: " + r.getResponseBody()))
+      }
+
+      Await.result(result, Duration(10, TimeUnit.SECONDS))
+    } catch {
+      case e: Throwable =>
+        Left(e)
+    }
+  }
+
 }
