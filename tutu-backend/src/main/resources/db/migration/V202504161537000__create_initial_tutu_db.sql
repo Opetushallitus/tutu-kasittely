19d7ddaf00c10b29d531a07502380fe45daf10b0
Tatu Honkanen <tatu.honkanen@rebase.fi>
2025-04-22 12:46:52 +0300

    OPHTUTU-24: Lisätty kommentit tauluille ja sarakkeille.

--- tutu-backend/src/main/resources/db/migration/V202504161537000__create_initial_tutu_db.sql
+++ tutu-backend/src/main/resources/db/migration/V202504161537000__create_initial_tutu_db.sql
@@ -20,6 +20,15 @@ CREATE TABLE IF NOT EXISTS esittelija (
     muokkaaja VARCHAR(255)
 );
 
+COMMENT ON TABLE esittelija IS 'Tutu-hakemusten esittelijät ja esittelijöiden vastuualueet';
+COMMENT ON COLUMN esittelija.id IS 'Taulun rivin id';
+COMMENT ON COLUMN esittelija.maatjavaltiot_koodi_uri IS 'Maatjavaltiot-koodiston koodiuri, joka on esittelijän vastuualuetta';
+COMMENT ON COLUMN esittelija.esittelija_oid IS 'Esittelijän henkilöOID';
+COMMENT ON COLUMN esittelija.luotu IS 'Taulun rivin luontiaika';
+COMMENT ON COLUMN esittelija.luotu IS 'Taulun rivin luoja';
+COMMENT ON COLUMN esittelija.muokattu IS 'Taulun rivin viimeisin muokkausaika';
+COMMENT ON COLUMN esittelija.muokkaaja IS 'Taulun rivin viimeisin muokkaaja';
+
 CREATE INDEX idx_esittelija_oid ON esittelija(esittelija_oid);
 CREATE INDEX idx_esittelija_maatjavaltiot_koodi_uri ON esittelija(maatjavaltiot_koodi_uri);
 
@@ -40,6 +49,17 @@ CREATE TABLE IF NOT EXISTS hakemus (
     muokattu TIMESTAMPTZ,
     muokkaaja VARCHAR(255)
 );
+
+COMMENT ON TABLE hakemus IS 'Tutu-hakemuksen tiedot';
+COMMENT ON COLUMN hakemus.id IS 'Taulun rivin id';
+COMMENT ON COLUMN hakemus.hakemus_oid IS 'Hakemuspalvelun hakemustunniste';
+COMMENT ON COLUMN hakemus.esittelija_id IS 'Esittelija-taulun esittelijä-id';
+COMMENT ON COLUMN hakemus.esitelty IS 'Hakemuksen esittelyaika';
+COMMENT ON COLUMN hakemus.paatos IS 'Hakemuksen päätöksen ajankohta';
+COMMENT ON COLUMN hakemus.luotu IS 'Taulun rivin luontiaika';
+COMMENT ON COLUMN hakemus.luotu IS 'Taulun rivin luoja';
+COMMENT ON COLUMN hakemus.muokattu IS 'Taulun rivin viimeisin muokkausaika';
+COMMENT ON COLUMN hakemus.muokkaaja IS 'Taulun rivin viimeisin muokkaaja';
 
 CREATE INDEX idx_hakemus_oid ON hakemus(hakemus_oid);
 CREATE INDEX idx_hakemus_esittelija ON hakemus(esittelija_id);
@@ -64,6 +84,15 @@ CREATE TABLE IF NOT EXISTS muistio (
     muokkaaja VARCHAR(255)
 );
 
+COMMENT ON TABLE muistio IS 'Tutu-hakemuksen muistiot';
+COMMENT ON COLUMN muistio.id IS 'Taulun rivin id';
+COMMENT ON COLUMN muistio.hakemus_id IS 'Hakemustaulun hakemuksen id';
+COMMENT ON COLUMN muistio.sisalto IS 'Muistion sisälto';
+COMMENT ON COLUMN muistio.luotu IS 'Taulun rivin luontiaika';
+COMMENT ON COLUMN muistio.luotu IS 'Taulun rivin luoja';
+COMMENT ON COLUMN muistio.muokattu IS 'Taulun rivin viimeisin muokkausaika';
+COMMENT ON COLUMN muistio.muokkaaja IS 'Taulun rivin viimeisin muokkaaja';
+
 CREATE INDEX idx_muistio_hakemus_id ON muistio(hakemus_id);
 
 ALTER TABLE muistio
@@ -86,6 +115,15 @@ CREATE TABLE IF NOT EXISTS paatos (
     muokattu TIMESTAMPTZ,
     muokkaaja VARCHAR(255)
 );
+
+COMMENT ON TABLE paatos IS 'Tutu-hakemuksen päätökset';
+COMMENT ON COLUMN paatos.id IS 'Taulun rivin id';
+COMMENT ON COLUMN paatos.hakemus_id IS 'Hakemustaulun hakemuksen id';
+COMMENT ON COLUMN paatos.sisalto IS 'Päätöksen sisälto';
+COMMENT ON COLUMN paatos.luotu IS 'Taulun rivin luontiaika';
+COMMENT ON COLUMN paatos.luotu IS 'Taulun rivin luoja';
+COMMENT ON COLUMN paatos.muokattu IS 'Taulun rivin viimeisin muokkausaika';
+COMMENT ON COLUMN paatos.muokkaaja IS 'Taulun rivin viimeisin muokkaaja';
 
 CREATE INDEX idx_paatos_hakemus ON paatos(hakemus_id);
 ALTER TABLE paatos
