a2547b64f890931230ad17c5bfdaeefd7e0b9d5d
Tatu Honkanen <tatu.honkanen@rebase.fi>
2025-05-12 13:33:12 +0300

    OPHTUTU-28: Mockkaa frontissa backend-kutsut

--- tutu-frontend/playwright/accessibility.spec.ts
+++ tutu-frontend/playwright/accessibility.spec.ts
@@ -6,6 +6,47 @@ const expectPageAccessibilityOk = async (page: Page) => {
   expect(accessibilityScanResults.violations).toEqual([]);
 };
 
+test.beforeEach(async ({ page }) => {
+  await page.route('**/tutu-backend/api/csrf', async (route) => {
+    await route.fulfill({
+      status: 200,
+      contentType: 'application/json',
+      body: JSON.stringify({
+        parameterName: '_csrf',
+        token:
+          'Gbt6oz01mVW5QV7XQ-hz_5P6BKJ_qkPb0xhjx6ZiW8uYyAnhKotKklkFqjGUJz-0dMVHyqqZKZscnSL24SsBpMRQOvn-rTCF',
+        headerName: 'X-CSRF-TOKEN',
+      }),
+    });
+  });
+  await page.route('**/tutu-backend/api/user', async (route) => {
+    await route.fulfill({
+      status: 200,
+      contentType: 'application/json',
+      body: JSON.stringify({
+        user: {
+          userOid: '1.2.246.562.24.999999999999',
+          authorities: [
+            'ROLE_APP_TUTU_ESITTELIJA',
+            'ROLE_APP_TUTU_ESITTELIJA_1.2.246.562.10.00000000001',
+          ],
+          asiointikieli: 'fi',
+        },
+      }),
+    });
+  });
+  await page.route('**/tutu-backend/api/session', async (route) => {
+    await route.fulfill({
+      status: 200,
+      contentType: 'application/json',
+      body: JSON.stringify({
+        empty: false,
+        traversableAgain: true,
+      }),
+    });
+  });
+});
+
 test('Saavutettavuus ok', async ({ page }) => {
   await page.goto('/');
   await expect(page.locator('h1')).toHaveText('Tutkintojen tunnustaminen');
