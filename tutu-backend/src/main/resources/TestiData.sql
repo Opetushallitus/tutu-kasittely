da6a6958796f15f6be0dc3c3b0f9f92983d85360
Tatu Honkanen <tatu.honkanen@rebase.fi>
2025-05-30 11:48:27 +0300

    OPHTUTU-48: Lisätty testidatan generointifile

---
+++ tutu-backend/src/main/resources/TestiData.sql
@@ -0,0 +1,15 @@
+--Replace <OWN_USER_OID> with your own OID from /cas/me
+
+-- Esittelija
+insert into esittelija (esittelija_oid, luoja, maatjavaltiot_koodi_uri) values ('<OWN_USER_OID>', 'test', '0008');
+
+-- Hakemus
+insert into hakemus (hakemus_oid, esittelija_id, hakemus_koskee, luoja) values ('1.2.246.562.11.00000000000002354802', (select id from esittelija where esittelija_oid = '<OWN_USER_OID>'), 0, 'test');
+insert into hakemus (hakemus_oid, esittelija_id, hakemus_koskee, luoja) values ('1.2.246.562.11.00000000000002354803', null, 1, 'test');
+insert into hakemus (hakemus_oid, esittelija_id, hakemus_koskee, luoja) values ('1.2.246.562.11.00000000000002353416', null, 2, 'test');
+insert into hakemus (hakemus_oid, esittelija_id, hakemus_koskee, luoja) values ('1.2.246.562.11.00000000000002354703', (select id from esittelija where esittelija_oid = '<OWN_USER_OID>'), 0, 'test');
+insert into hakemus (hakemus_oid, esittelija_id, hakemus_koskee, luoja) values ('1.2.246.562.11.00000000000002354736', (select id from esittelija where esittelija_oid = '<OWN_USER_OID>'), 1, 'test');
+insert into hakemus (hakemus_oid, esittelija_id, hakemus_koskee, luoja) values ('1.2.246.562.11.00000000000002354835', (select id from esittelija where esittelija_oid = '<OWN_USER_OID>'), 2, 'test');
+insert into hakemus (hakemus_oid, esittelija_id, hakemus_koskee, luoja) values ('1.2.246.562.11.00000000000002354867', (select id from esittelija where esittelija_oid = '<OWN_USER_OID>'), 3, 'test');
+insert into hakemus (hakemus_oid, esittelija_id, hakemus_koskee, luoja) values ('1.2.246.562.11.00000000000002354702', (select id from esittelija where esittelija_oid = '<OWN_USER_OID>'), 0, 'test');
+insert into hakemus (hakemus_oid, esittelija_id, hakemus_koskee, luoja) values ('1.2.246.562.11.99999999999999999999', (select id from esittelija where esittelija_oid = '<OWN_USER_OID>'), 1, 'test');
