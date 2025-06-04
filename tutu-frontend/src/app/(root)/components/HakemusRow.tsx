d710f657b549e30fb565f1cbe6d4002b8b4d792e
Joel Luukka <joel.luukka@knowit.fi>
2025-06-04 15:44:44 +0300

    OPHTUTU-41: Lisätty hakemuksen kokonaisaika hakemuslistaukseen

--- tutu-frontend/src/app/(root)/components/HakemusRow.tsx
+++ tutu-frontend/src/app/(root)/components/HakemusRow.tsx
@@ -3,10 +3,34 @@ import { styled, TableCell, TableRow } from '@mui/material';
 import Link from 'next/link';
 import { hakemusKoskeeOptions } from '@/src/constants/dropdownOptions';
 import { useTranslations } from '@/src/lib/localization/useTranslations';
+import * as dateFns from 'date-fns';
 
 const StyledTableCell = styled(TableCell)({
   borderBottom: 'none',
 });
+
+export const muotoileKokonaisaikaPure = (nyt: Date, luotu: Date) => {
+  const kuukausiYlitetty = nyt.getDate() - luotu.getDate() < 0;
+
+  const jaksonAlku = new Date(
+    dateFns.subMonths(nyt, kuukausiYlitetty ? 1 : 0).setDate(luotu.getDate()),
+  );
+  const paiviaKuluvassaJaksossa = dateFns.getDaysInMonth(jaksonAlku);
+
+  const erotusKk = dateFns.differenceInMonths(nyt, luotu);
+  const erotusPv = dateFns.differenceInCalendarDays(nyt, jaksonAlku);
+
+  const aikaDesimaalina = erotusKk + ((erotusPv / paiviaKuluvassaJaksossa) % 1);
+
+  return aikaDesimaalina.toFixed(1);
+};
+
+export const muotoileKokonaisaika = (luotuStr: string) => {
+  const luotu = new Date(luotuStr);
+  const nyt = new Date();
+
+  return muotoileKokonaisaikaPure(nyt, luotu);
+};
 
 export default function HakemusRow({
   hakemus,
@@ -37,7 +61,11 @@ export default function HakemusRow({
       )}
       <StyledTableCell>{hakemus.vaihe}</StyledTableCell>
       <StyledTableCell>{t(hakemusKoskee)}</StyledTableCell>
-      <StyledTableCell>{hakemus.aika}</StyledTableCell>
+      <StyledTableCell>
+        {t('hakemuslista.kokonaisaika.arvo', '', {
+          aika: muotoileKokonaisaika(hakemus.aika),
+        })}
+      </StyledTableCell>
     </TableRow>
   );
 }
