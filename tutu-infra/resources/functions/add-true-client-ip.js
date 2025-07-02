83b09194ca226c0f02e3074788968fc1bac7e5d8
Joona Haavisto <joona.haavisto@gofore.com>
2025-07-02 19:11:46 +0300

    Korjattu typo

--- tutu-infra/resources/functions/add-true-client-ip.js
+++
@@ -1,9 +0,0 @@
-async function handler(event) {
-  var request = event.request;
-  var clientIP = event.viewer.ip;
-
-  //Add the true-client-ip header to the incoming request
-  request.headers['True-Client-IP'] = {value: clientIP};
-
-  return request;
-}
