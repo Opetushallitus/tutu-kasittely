f00b9168e25b3edfbbcc92c816cc423ad4691bf4
Joona Haavisto <joona.haavisto@gofore.com>
2025-07-11 12:45:33 +0300

    Precommit testi

--- tutu-frontend/.lintstagedrc.mjs
+++ tutu-frontend/.lintstagedrc.mjs
@@ -1,9 +1,11 @@
 const eslintCommand = 'eslint --fix --no-ignore --max-warnings=0';
 const prettierCommand = 'prettier --write -u';
+const spotlessCommand = 'cd ../tutu-backend && mvn spotless:apply';
 
 const config = {
-  '**/*.{js,mjs,cjs,jsx,ts,tsx}': [eslintCommand, prettierCommand],
-  '!**/*.{js,mjs,cjs,jsx,ts,tsx}': prettierCommand,
+  '../**/*.{js,mjs,cjs,jsx,ts,tsx}': [eslintCommand, prettierCommand],
+  '../!**/*.{js,mjs,cjs,jsx,ts,tsx}': prettierCommand,
+  '../**/*.{scala}': spotlessCommand,
 };
 
 export default config;
