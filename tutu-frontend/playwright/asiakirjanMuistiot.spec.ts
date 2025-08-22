8ef64ca55d6234fa48d51e13be01b888b1c345fc
Joel Luukka <joel.luukka@knowit.fi>
2025-08-22 11:07:35 +0300

    OPHTUTU-99: Muistio testit (playwright)

---
+++ tutu-frontend/playwright/asiakirjanMuistiot.spec.ts
@@ -0,0 +1,54 @@
+import { expect, test } from '@playwright/test';
+import {
+  mockBasicForHakemus,
+  mockHakemus,
+  mockLiitteet,
+  mockUser,
+} from '@/playwright/mocks';
+
+test.beforeEach(mockBasicForHakemus);
+
+test('Asiakirjan sisäisen muistion esittäminen ja tallennus', async ({
+  page,
+}) => {
+  await mockUser(page);
+  await mockHakemus(page);
+  await mockLiitteet(page);
+
+  await page.route(`**/muistio/**`, async (route) => {
+    if (route.request().method() === 'GET') {
+      route.fulfill({
+        status: 200,
+        contentType: 'application/json',
+        body: JSON.stringify({
+          id: 'mock_uuid_1',
+          hakemus_id: 'mock_uuid_2',
+          sisalto: 'Muistion alkuperäinen sisältö',
+          luotu: '2025-08-21T12:52:00',
+          luoja: 'Hakemuspalvelu',
+          muokattu: undefined,
+          muokkaaja: undefined,
+          sisainenHuomio: true,
+          hakemuksenOsa: 'asiakirjat',
+        }),
+      });
+    }
+  });
+
+  await page.goto(
+    '/tutu-frontend/hakemus/1.2.246.562.10.00000000001/asiakirjat',
+  );
+  const muistio = page.getByTestId('muistio-asiakirjat-sisainen');
+
+  await expect(muistio).toBeVisible();
+  await expect(muistio).toContainText('Muistion alkuperäinen sisältö');
+
+  const [request] = await Promise.all([
+    page.waitForRequest(
+      (req) => req.url().includes(`/muistio/`) && req.method() === 'POST',
+    ),
+    muistio.fill('Parempaa sisältöä!'),
+  ]);
+
+  expect(request.postDataJSON().sisalto).toEqual('Parempaa sisältöä!');
+});
