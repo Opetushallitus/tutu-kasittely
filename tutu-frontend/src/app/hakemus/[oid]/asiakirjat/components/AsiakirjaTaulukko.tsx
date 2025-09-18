0a09b2de44a415dc2f5b9ba9c10ffa985c5c5818
Matti Pitkämäki <matti.pitkamaki@gofore.com>
2025-09-18 08:56:22 +0300

    OPHTUTU-204: Poistettu turha debug-logitus

--- tutu-frontend/src/app/hakemus/[oid]/asiakirjat/components/AsiakirjaTaulukko.tsx
+++ tutu-frontend/src/app/hakemus/[oid]/asiakirjat/components/AsiakirjaTaulukko.tsx
@@ -215,7 +215,6 @@ export const haeAsiakirjat = (sisalto: SisaltoPathNode[]): SisaltoValue[] => {
 
   traverseSisaltoTree(sisalto, handleItem);
 
-  console.log('!!!!!!!!!!!!! ', sisalto);
   return acc;
 };
 
