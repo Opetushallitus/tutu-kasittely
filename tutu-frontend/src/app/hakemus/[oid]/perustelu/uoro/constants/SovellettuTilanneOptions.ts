bcf401c6153a48eff18d3b535ce77a8fd7c416e9
Tatu Honkanen <tatu.honkanen@rebase.fi>
2025-09-18 12:10:01 +0300

    OPHTUTU-226: UO/RO: UI-komponentit

---
+++ tutu-frontend/src/app/hakemus/[oid]/perustelu/uoro/constants/SovellettuTilanneOptions.ts
@@ -0,0 +1,86 @@
+export const sovellettuTilanneOptions = [
+  { value: 'A', label: 'A' },
+  { value: 'B', label: 'B' },
+  { value: 'C', label: 'C' },
+  { value: 'D', label: 'D' },
+];
+
+export const sovellettuTilanneOpetettavatAineetVieraatKieletOptions = [
+  { value: 'A1', label: 'A1' },
+  { value: 'A2', label: 'A2' },
+  { value: 'A3', label: 'A3' },
+  { value: 'A4', label: 'A4' },
+  { value: 'A5', label: 'A5' },
+  { value: 'B1', label: 'B1' },
+  { value: 'B2', label: 'B2' },
+  { value: 'B3', label: 'B3' },
+];
+
+export const SovellettuTilanneOpetettavatAineetOptions = {
+  liikunta: [
+    { value: 'A', label: 'A' },
+    { value: 'B', label: 'B' },
+    { value: 'C', label: 'C' },
+    { value: 'D', label: 'D' },
+    { value: 'E', label: 'E' },
+  ],
+  musiikki: [
+    { value: 'A', label: 'A' },
+    { value: 'B', label: 'B' },
+    { value: 'C', label: 'C' },
+    { value: 'D', label: 'D' },
+    { value: 'E', label: 'E' },
+    { value: 'F', label: 'F' },
+  ],
+  kuvataide: [
+    { value: 'A', label: 'A' },
+    { value: 'B', label: 'B' },
+    { value: 'C', label: 'C' },
+    { value: 'D', label: 'D' },
+    { value: 'E', label: 'E' },
+    { value: 'F', label: 'F' },
+  ],
+  kemia: [
+    { value: 'A', label: 'A' },
+    { value: 'B', label: 'B' },
+    { value: 'C', label: 'C' },
+    { value: 'D', label: 'D' },
+  ],
+  matematiikka: [
+    { value: 'A', label: 'A' },
+    { value: 'B', label: 'B' },
+    { value: 'C', label: 'C' },
+    { value: 'D', label: 'D' },
+  ],
+  fysiikka: [
+    { value: 'A', label: 'A' },
+    { value: 'B', label: 'B' },
+    { value: 'C', label: 'C' },
+    { value: 'D', label: 'D' },
+  ],
+  biologia: [
+    { value: 'A', label: 'A' },
+    { value: 'B', label: 'B' },
+    { value: 'C', label: 'C' },
+    { value: 'D', label: 'D' },
+  ],
+  maantiede: [
+    { value: 'A', label: 'A' },
+    { value: 'B', label: 'B' },
+    { value: 'C', label: 'C' },
+    { value: 'D', label: 'D' },
+  ],
+  historia: [
+    { value: 'A', label: 'A' },
+    { value: 'A1', label: 'A1' },
+    { value: 'A2', label: 'A2' },
+    { value: 'A3', label: 'A3' },
+    { value: 'B', label: 'B' },
+    { value: 'B1', label: 'B1' },
+  ],
+  yhteiskuntaoppi: [
+    { value: 'A', label: 'A' },
+    { value: 'A1', label: 'A1' },
+    { value: 'B', label: 'B' },
+  ],
+};
