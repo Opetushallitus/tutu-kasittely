14e568a1e20b341a6d3268e1e86ea68b8d759c52
Tatu Honkanen <tatu.honkanen@rebase.fi>
2025-09-19 14:33:15 +0300

    OPHTUTU-226: Migraatio perustelutaulujen yhdistämisille.

--- tutu-backend/src/main/resources/TestiData.sql
+++ tutu-backend/src/main/resources/TestiData.sql
@@ -30,23 +30,23 @@ insert into hakemus (hakemus_oid, esittelija_id, asiakirja_id, hakemus_koskee, l
 insert into hakemus (hakemus_oid, esittelija_id, asiakirja_id, hakemus_koskee, luoja) values ('1.2.246.562.11.00000000000002355032', (select id from esittelija where esittelija_oid = '<OWN_USER_OID>'), (select id from asiakirja limit 1 offset 8), 0, 'test');
 insert into hakemus (hakemus_oid, esittelija_id, asiakirja_id, hakemus_koskee, luoja) values ('1.2.246.562.11.99999999999999999999', (select id from esittelija where esittelija_oid = '<OWN_USER_OID>'), (select id from asiakirja limit 1 offset 9), 1, 'test');
 
-insert into perustelu_yleiset (hakemus_id, selvitys_tutkinnon_asemasta_lahtomaan_jarjestelmassa, selvitys_tutkinnon_myontajasta_ja_tutkinnon_virallisuudesta, luoja)
+insert into perustelu (hakemus_id, selvitys_tutkinnon_asemasta_lahtomaan_jarjestelmassa, selvitys_tutkinnon_myontajasta_ja_tutkinnon_virallisuudesta, luoja)
     values ((select id from hakemus where hakemus_oid = '1.2.246.562.11.00000000000002354802'), '', '', 'testi');
-insert into perustelu_yleiset (hakemus_id, selvitys_tutkinnon_asemasta_lahtomaan_jarjestelmassa, selvitys_tutkinnon_myontajasta_ja_tutkinnon_virallisuudesta, luoja)
+insert into perustelu (hakemus_id, selvitys_tutkinnon_asemasta_lahtomaan_jarjestelmassa, selvitys_tutkinnon_myontajasta_ja_tutkinnon_virallisuudesta, luoja)
     values ((select id from hakemus where hakemus_oid = '1.2.246.562.11.00000000000002354803'), '', '', 'testi');
-insert into perustelu_yleiset (hakemus_id, selvitys_tutkinnon_asemasta_lahtomaan_jarjestelmassa, selvitys_tutkinnon_myontajasta_ja_tutkinnon_virallisuudesta, luoja)
+insert into perustelu (hakemus_id, selvitys_tutkinnon_asemasta_lahtomaan_jarjestelmassa, selvitys_tutkinnon_myontajasta_ja_tutkinnon_virallisuudesta, luoja)
     values ((select id from hakemus where hakemus_oid = '1.2.246.562.11.00000000000002353416'), '', '', 'testi');
-insert into perustelu_yleiset (hakemus_id, selvitys_tutkinnon_asemasta_lahtomaan_jarjestelmassa, selvitys_tutkinnon_myontajasta_ja_tutkinnon_virallisuudesta, luoja)
+insert into perustelu (hakemus_id, selvitys_tutkinnon_asemasta_lahtomaan_jarjestelmassa, selvitys_tutkinnon_myontajasta_ja_tutkinnon_virallisuudesta, luoja)
     values ((select id from hakemus where hakemus_oid = '1.2.246.562.11.00000000000002354703'), '', '', 'testi');
-insert into perustelu_yleiset (hakemus_id, selvitys_tutkinnon_asemasta_lahtomaan_jarjestelmassa, selvitys_tutkinnon_myontajasta_ja_tutkinnon_virallisuudesta, luoja)
+insert into perustelu (hakemus_id, selvitys_tutkinnon_asemasta_lahtomaan_jarjestelmassa, selvitys_tutkinnon_myontajasta_ja_tutkinnon_virallisuudesta, luoja)
     values ((select id from hakemus where hakemus_oid = '1.2.246.562.11.00000000000002354736'), '', '', 'testi');
-insert into perustelu_yleiset (hakemus_id, selvitys_tutkinnon_asemasta_lahtomaan_jarjestelmassa, selvitys_tutkinnon_myontajasta_ja_tutkinnon_virallisuudesta, luoja)
+insert into perustelu (hakemus_id, selvitys_tutkinnon_asemasta_lahtomaan_jarjestelmassa, selvitys_tutkinnon_myontajasta_ja_tutkinnon_virallisuudesta, luoja)
     values ((select id from hakemus where hakemus_oid = '1.2.246.562.11.00000000000002354835'), '', '', 'testi');
-insert into perustelu_yleiset (hakemus_id, selvitys_tutkinnon_asemasta_lahtomaan_jarjestelmassa, selvitys_tutkinnon_myontajasta_ja_tutkinnon_virallisuudesta, luoja)
+insert into perustelu (hakemus_id, selvitys_tutkinnon_asemasta_lahtomaan_jarjestelmassa, selvitys_tutkinnon_myontajasta_ja_tutkinnon_virallisuudesta, luoja)
     values ((select id from hakemus where hakemus_oid = '1.2.246.562.11.00000000000002354867'), '', '', 'testi');
-insert into perustelu_yleiset (hakemus_id, selvitys_tutkinnon_asemasta_lahtomaan_jarjestelmassa, selvitys_tutkinnon_myontajasta_ja_tutkinnon_virallisuudesta, luoja)
+insert into perustelu (hakemus_id, selvitys_tutkinnon_asemasta_lahtomaan_jarjestelmassa, selvitys_tutkinnon_myontajasta_ja_tutkinnon_virallisuudesta, luoja)
     values ((select id from hakemus where hakemus_oid = '1.2.246.562.11.00000000000002354702'), '', '', 'testi');
-insert into perustelu_yleiset (hakemus_id, selvitys_tutkinnon_asemasta_lahtomaan_jarjestelmassa, selvitys_tutkinnon_myontajasta_ja_tutkinnon_virallisuudesta, luoja)
+insert into perustelu (hakemus_id, selvitys_tutkinnon_asemasta_lahtomaan_jarjestelmassa, selvitys_tutkinnon_myontajasta_ja_tutkinnon_virallisuudesta, luoja)
     values ((select id from hakemus where hakemus_oid = '1.2.246.562.11.00000000000002355032'), '', '', 'testi');
-insert into perustelu_yleiset (hakemus_id, selvitys_tutkinnon_asemasta_lahtomaan_jarjestelmassa, selvitys_tutkinnon_myontajasta_ja_tutkinnon_virallisuudesta, luoja)
+insert into perustelu (hakemus_id, selvitys_tutkinnon_asemasta_lahtomaan_jarjestelmassa, selvitys_tutkinnon_myontajasta_ja_tutkinnon_virallisuudesta, luoja)
     values ((select id from hakemus where hakemus_oid = '1.2.246.562.11.99999999999999999999'), '', '', 'testi');
