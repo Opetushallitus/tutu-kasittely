d73ece26ec2456d37ee1ad7ef91d50e7e1759513
Joel Luukka <joel.luukka@knowit.fi>
2025-09-29 15:15:20 +0300

    OPHTUTU-242: Added data transfer from old database structure to new one

--- tutu-backend/src/main/resources/db/migration/V202509291219000__add_text_fields_to_perustelu.sql
+++ tutu-backend/src/main/resources/db/migration/V202509291219000__add_text_fields_to_perustelu.sql
@@ -1,4 +1,61 @@
+-- Add muu perustelu field to perustelu
 ALTER TABLE perustelu
     ADD COLUMN IF NOT EXISTS muu_perustelu TEXT NOT NULL DEFAULT '';
 
 COMMENT ON COLUMN perustelu.muu_perustelu IS 'Myy yleinen perustelu';
+
+-- Copy data from muistio to perustelu
+UPDATE perustelu
+SET muu_perustelu = mu.sisalto
+FROM muistio mu
+WHERE
+  perustelu.hakemus_id = mu.hakemus_id
+AND
+  mu.sisainen_huomio = FALSE
+AND
+  mu.hakemuksen_osa = 'perustelut-yleiset--muu-perustelu';
+
+UPDATE perustelu
+SET selvitys_tutkinnon_asemasta_lahtomaan_jarjestelmassa = mu.sisalto
+FROM muistio mu
+WHERE
+  perustelu.hakemus_id = mu.hakemus_id
+AND
+  mu.sisainen_huomio = FALSE
+AND
+  mu.hakemuksen_osa = 'perustelut-yleiset--selvitys-tutkinnon-asemasta';
+
+UPDATE perustelu
+SET selvitys_tutkinnon_myontajasta_ja_tutkinnon_virallisuudesta = mu.sisalto
+FROM muistio mu
+WHERE
+  perustelu.hakemus_id = mu.hakemus_id
+AND
+  mu.sisainen_huomio = FALSE
+AND
+  mu.hakemuksen_osa = 'perustelut-yleiset--selvitys-tutkinnon-myontajasta';
+
+-- Delete unnecessary muistio data
+DELETE FROM muistio
+WHERE
+  perustelu.hakemus_id = mu.hakemus_id
+AND
+  mu.sisainen_huomio = FALSE
+AND
+  mu.hakemuksen_osa = 'perustelut-yleiset--muu-perustelu';
+
+DELETE FROM muistio
+WHERE
+  perustelu.hakemus_id = mu.hakemus_id
+AND
+  mu.sisainen_huomio = FALSE
+AND
+  mu.hakemuksen_osa = 'perustelut-yleiset--selvitys-tutkinnon-asemasta';
+
+DELETE FROM muistio
+WHERE
+  perustelu.hakemus_id = mu.hakemus_id
+AND
+  mu.sisainen_huomio = FALSE
+AND
+  mu.hakemuksen_osa = 'perustelut-yleiset--selvitys-tutkinnon-myontajasta';
