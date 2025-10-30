50440f181b12f0edf8fdea6399f552fc88140df1
Henri Lehtinen <henri.a.lehtinen@outlook.com>
2025-10-30 13:50:44 +0200

    Lisää controller package object API response kuvauksille
    Luo controller package object, joka sisältää vakiot API response
    kuvauksille (200, 400, 403, 500). Nämä vakiot olivat viittailtu
    PerusteluController ja PaatosController @Operation annotaatioissa,
    mutta eivät olleet määriteltyjä missään, aiheuttaen käännösvirheen.
    
    Package object mahdollistaa:
    - DRY periaate: yksi lähde kaikille response kuvauksille
    - Helppo ylläpidettävyys
    - Automaattinen saatavuus kaikille controller paketin luokille
    - Yhtenäiset virheilmoitukset eri controller luokissa
    
    Korjaa: Käännösvirhe joka johtui määrittelemättömistä vakioista

--- tutu-backend/src/main/scala/fi/oph/tutu/backend/controller/package.scala
+++ tutu-backend/src/main/scala/fi/oph/tutu/backend/controller/package.scala
@@ -1,10 +1,7 @@
 package fi.oph.tutu.backend
 
-package object controller {
-  final val RESPONSE_200_DESCRIPTION =
-    "Pyyntö vastaanotettu"
-  final val RESPONSE_400_DESCRIPTION = "Pyyntö virheellinen"
-  final val RESPONSE_403_DESCRIPTION =
-    "Käyttäjällä ei ole tarvittavia oikeuksia hakemusten luontiin"
+package object controller:
+  final val RESPONSE_200_DESCRIPTION = "Pyyntö onnistui"
+  final val RESPONSE_400_DESCRIPTION = "Virheellinen pyyntö"
+  final val RESPONSE_403_DESCRIPTION = "Ei käyttöoikeuksia"
   final val RESPONSE_500_DESCRIPTION = "Palvelinvirhe"
-}
