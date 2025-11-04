26af25bdb9200e47727d791f589f676be4b72321
Joona Haavisto <joona.haavisto@gofore.com>
2025-11-04 13:34:54 +0200

    Lintteri korjauksia ja puuttuva testi

--- tutu-frontend/playwright/asiakirjojenvahvistaminen.spec.ts
+++ tutu-frontend/playwright/asiakirjojenvahvistaminen.spec.ts
@@ -128,7 +128,8 @@ test('Valmistumisen vahvistus -komponentit toimivat oikein', async ({
     .locator(
       '.react-datepicker__day--026:not(.react-datepicker__day--outside-month)',
     )
-    .last().click();
+    .last()
+    .click();
   await expect(lahetettyInput).toHaveValue(
     dateFns.format(new Date().setDate(26), 'dd.MM.yyyy'),
   );
@@ -139,7 +140,8 @@ test('Valmistumisen vahvistus -komponentit toimivat oikein', async ({
     .locator(
       '.react-datepicker__day--026:not(.react-datepicker__day--outside-month)',
     )
-    .last().click();
+    .last()
+    .click();
   await expect(vastattuInput).toHaveValue(
     dateFns.format(new Date().setDate(26), 'dd.MM.yyyy'),
   );
