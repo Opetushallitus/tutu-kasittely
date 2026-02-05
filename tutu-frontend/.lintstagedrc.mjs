5518111fe1fc23ceb275b64069d7c6ec78d5b725
Joona Haavisto <joona.haavisto@gofore.com>
2026-02-05 14:07:51 +0200

    Erotettu playwright omaksi composite actioniksi ja tsc precommitiin

--- tutu-frontend/.lintstagedrc.mjs
+++ tutu-frontend/.lintstagedrc.mjs
@@ -1,7 +1,9 @@
 const eslintCommand = 'eslint --fix --no-ignore --max-warnings=0';
 const prettierCommand = 'prettier --write -u';
+const typecheckerCommand = 'tsc --noEmit';
 
 const config = {
+  '**/*.ts?(x)': () => typecheckerCommand,
   '**/*.{js,mjs,cjs,jsx,ts,tsx}': [eslintCommand, prettierCommand],
   '!**/*.{js,mjs,cjs,jsx,ts,tsx}': prettierCommand,
 };
