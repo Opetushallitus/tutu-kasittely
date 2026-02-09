bd76c934fe586f5adfc51bd8ee1a15ea14f1842f
Juha Lahtonen <juha.lahtonen@gofore.com>
2026-02-09 11:12:56 +0200

    OPHTUTU-83 Yhteisen käsittelyn sivun perusrakenne

--- tutu-frontend/src/app/(root)/components/JointProcessingPage.tsx
+++ tutu-frontend/src/app/(root)/components/JointProcessingPage.tsx
@@ -1,6 +1,39 @@
+import { useState } from 'react';
+
+import { Tabs } from '@/src/app/(root)/components/Tabs';
+import { BoxWrapper } from '@/src/components/BoxWrapper';
 import { User } from '@/src/lib/types/user';
 
 export default function JointProcessingPage({ user }: { user: User | null }) {
+  const [tab, setTab] = useState<string>('saapuneet');
+
+  const handleTabChange = (newTab: string) => () => {
+    if (newTab !== tab) {
+      setTab(newTab);
+    }
+  };
+
   console.log('User: ', user);
-  return <></>;
+
+  return (
+    <BoxWrapper>
+      <Tabs
+        tPrefix="yhteinenKasittely"
+        buttons={[
+          {
+            onClick: handleTabChange('saapuneet'),
+            tabName: 'saapuneet',
+            active: tab === 'saapuneet',
+            value: 3,
+          },
+          {
+            onClick: handleTabChange('lahetetyt'),
+            tabName: 'lahetetyt',
+            active: tab === 'lahetetyt',
+            value: 2,
+          },
+        ]}
+      />
+    </BoxWrapper>
+  );
 }
