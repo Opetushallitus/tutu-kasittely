84319e7e03a26df8761a5e364690b540e2ac7937
Juha Lahtonen <juha.lahtonen@gofore.com>
2026-02-11 11:22:47 +0200

    OPHTUTU-83 Lisätty komponentit lähetetyille ja vastaanotetuille viesteille

--- tutu-frontend/src/app/(root)/components/JointProcessingPage.tsx
+++ tutu-frontend/src/app/(root)/components/JointProcessingPage.tsx
@@ -1,9 +1,38 @@
 import { useState } from 'react';
 
+import ReceivedMessages from '@/src/app/(root)/components/JointProcessing/ReceivedMessages';
+import SentMessages from '@/src/app/(root)/components/JointProcessing/SentMessages';
 import { Tabs } from '@/src/app/(root)/components/Tabs';
 import { BoxWrapper } from '@/src/components/BoxWrapper';
 import { User } from '@/src/lib/types/user';
+import { YhteisenKasittelynViesti } from '@/src/lib/types/yhteisenKasittelynViesti';
 
+const mockReceived: YhteisenKasittelynViesti[] = [
+  {
+    hakemusOid: '123192',
+    asiatunnus: 'OPH-666-1234',
+    lahetysPvm: '2.2.2026',
+    lahettajaOid: '123123',
+    lahettaja: 'Ossi Orava',
+    vastaanottajaOid: '321312',
+    vastaanottaja: 'Kalle Kaniini',
+    hakijanNimi: 'Hessu Hirvi',
+    viesti: 'Lore ipsum',
+    vastaus: undefined,
+  },
+  {
+    hakemusOid: '123191',
+    asiatunnus: 'OPH-666-1233',
+    lahetysPvm: '1.2.2026',
+    lahettajaOid: '123123',
+    lahettaja: 'Ossi Orava',
+    vastaanottajaOid: '321312',
+    vastaanottaja: 'Kalle Kaniini',
+    hakijanNimi: 'Jaakko Jänö',
+    viesti: 'Lore ipsum',
+    vastaus: 'dolor sit',
+  },
+];
 export default function JointProcessingPage({ user }: { user: User | null }) {
   const [tab, setTab] = useState<string>('saapuneet');
 
@@ -12,8 +41,6 @@ export default function JointProcessingPage({ user }: { user: User | null }) {
       setTab(newTab);
     }
   };
-
-  console.log('User: ', user);
 
   return (
     <BoxWrapper>
@@ -34,6 +61,11 @@ export default function JointProcessingPage({ user }: { user: User | null }) {
           },
         ]}
       />
+      {tab === 'lahetetyt' ? (
+        <SentMessages messageList={mockReceived} user={user} />
+      ) : (
+        <ReceivedMessages messageList={mockReceived} user={user} />
+      )}
     </BoxWrapper>
   );
 }
