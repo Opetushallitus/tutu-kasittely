6207be41c45eea583c650a9b7da933cb8708ecc8
Matti Pitkämäki <matti.pitkamaki@gofore.com>
2026-03-03 10:52:32 +0200

    OPHTUTU-83: Korjattu käännösongelmia

--- tutu-backend/src/main/scala/fi/oph/tutu/backend/service/HakemusService.scala
+++ tutu-backend/src/main/scala/fi/oph/tutu/backend/service/HakemusService.scala
@@ -581,7 +581,6 @@ class HakemusService(
   def haeYkViestiLista(
     userOid: String,
     sort: String
-  ): Seq[YkViestiListItem] = {
-    val viestiLista = hakemusRepository.haeYkViestiLista(userOid)
-  }
+  ): Seq[YkViestiListItem] =
+    hakemusRepository.haeYkViestiLista(userOid)
 }
