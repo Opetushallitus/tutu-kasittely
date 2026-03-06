ea254c45d57ffb5199ca0eb42d0ff52fe1db4197
Juha Lahtonen <juha.lahtonen@gofore.com>
2026-03-06 15:45:09 +0200

    OPHTUTU-83 Lisätty hakemusOid hakutuloksiin

--- tutu-backend/src/main/scala/fi/oph/tutu/backend/repository/HakemusRepository.scala
+++ tutu-backend/src/main/scala/fi/oph/tutu/backend/repository/HakemusRepository.scala
@@ -79,6 +79,7 @@ class HakemusRepository extends BaseResultHandlers {
       YkViestiListItem(
         id = r.nextObject().asInstanceOf[UUID],
         hakemusId = r.nextObject().asInstanceOf[UUID],
+        hakemusOid = r.nextString(),
         asiatunnus = Option(r.nextString()),
         lahettajaOid = Option(r.nextString()),
         vastaanottajaOid = Option(r.nextString()),
@@ -474,6 +475,7 @@ class HakemusRepository extends BaseResultHandlers {
             v.id,
             v.hakemus_id,
             h.asiatunnus,
+            h.hakemus_oid,
             v.lahettaja_oid,
             v.vastaanottaja_oid,
             v.luotu,
