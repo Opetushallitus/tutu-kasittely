462fa937a17e467e6f2f398f4bb403dea6d8678d
Juha Lahtonen <juha.lahtonen@gofore.com>
2026-03-19 14:42:51 +0200

    OPHTUTU-83 Lisätty uudet api kutsut hookiin

--- tutu-frontend/src/hooks/useYkViestilista.ts
+++ tutu-frontend/src/hooks/useYkViestilista.ts
@@ -3,21 +3,40 @@ import { useQuery } from '@tanstack/react-query';
 import { doApiFetch } from '@/src/lib/tutu-backend/api';
 import { YhteisenKasittelynViesti } from '@/src/lib/types/yhteisenKasittelynViesti';
 
-export const getYkViestilista = async (): Promise<
+export const getYkSentMessages = async (): Promise<
   YhteisenKasittelynViesti[]
 > => {
   const localStorageSearchParams = localStorage.getItem('tutu-query-string');
   const url = localStorageSearchParams
-    ? `ykviestilista?${localStorageSearchParams}`
-    : `ykviestilista?sort?saapumisPvm:desc`;
+    ? `ykLahetetytViestit?${localStorageSearchParams}`
+    : `ykLahetetytViestit?sort?saapumisPvm:desc`;
 
   return await doApiFetch(url, undefined, 'no-store');
 };
 
-export const useYkViestilista = () =>
+export const getYkReceivedMessages = async (): Promise<
+  YhteisenKasittelynViesti[]
+> => {
+  const localStorageSearchParams = localStorage.getItem('tutu-query-string');
+  const url = localStorageSearchParams
+    ? `ykSaapuneetViestit?${localStorageSearchParams}`
+    : `ykSaapuneetViestit?sort?saapumisPvm:desc`;
+
+  return await doApiFetch(url, undefined, 'no-store');
+};
+
+export const useYkGetSentMessages = () =>
   useQuery({
-    queryKey: ['getYkViestilista'],
-    queryFn: getYkViestilista,
+    queryKey: ['getYkSentMessages'],
+    queryFn: getYkSentMessages,
+    refetchOnMount: 'always',
+    throwOnError: false,
+  });
+
+export const useYkGetReceivedMessages = () =>
+  useQuery({
+    queryKey: ['getYkReceivedMessages'],
+    queryFn: getYkReceivedMessages,
     refetchOnMount: 'always',
     throwOnError: false,
   });
