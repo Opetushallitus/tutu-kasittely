93a72e3cd48ac430823d0c141a4c4e969c5cf62d
Matti Pitkämäki <matti.pitkamaki@gofore.com>
2026-03-19 16:12:45 +0200

    OPHTUTU-177: Lisätty modaali vahvistetun viestin näyttämiseen

---
+++ tutu-frontend/src/app/hakemus/[oid]/editori/viesti/components/VahvistettuViestiModal.tsx
@@ -0,0 +1,165 @@
+import { EditOutlined, CopyAll, DeleteOutlined } from '@mui/icons-material';
+import CloseIcon from '@mui/icons-material/Close';
+import { Divider, Stack, styled } from '@mui/material';
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import { Theme } from '@mui/material/styles';
+import {
+  OphButton,
+  ophColors,
+  OphTypography,
+} from '@opetushallitus/oph-design-system';
+import React, { useEffect } from 'react';
+
+import { convertHtmlToMarkdown } from '@/src/app/hakemus/[oid]/editori/components/editor-utils';
+import { FullSpinner } from '@/src/components/FullSpinner';
+import useToaster, { AddToastCallback } from '@/src/hooks/useToaster';
+import { useVahvistettuViesti } from '@/src/hooks/useVahvistetutViestit';
+import { TFunction } from '@/src/lib/localization/hooks/useTranslations';
+import { Viestityyppi } from '@/src/lib/types/viesti';
+import { handleFetchError } from '@/src/lib/utils';
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  minWidth: 800,
+  height: '100%',
+  bgcolor: ophColors.white,
+  border: `00px solid ${ophColors.white}`,
+  boxShadow: 24,
+  p: 4,
+};
+
+const ViestiContent = styled(Box)({
+  whiteSpace: 'pre-line',
+  overflowY: 'auto',
+});
+
+export type ViestiMetadata = {
+  id: string;
+  tyyppi: Viestityyppi;
+};
+
+export type VahvistettuViestiModalProps = {
+  t: TFunction;
+  theme: Theme;
+  open: boolean;
+  viestiMetadata?: ViestiMetadata;
+  handleClose: () => void;
+  handlePoistaViesti: (id: string) => void;
+  handleLisaaEditoriin: (html: string) => void;
+};
+
+const handleCopy = (html: string, t: TFunction, addToast: AddToastCallback) => {
+  const markdown = convertHtmlToMarkdown(html);
+  navigator.clipboard.writeText(markdown);
+  addToast({
+    key: 'hakemus.viesti.kopioi.toaster',
+    message: t('hakemus.viesti.kopioituToast'),
+    type: 'success',
+    timeMs: 2500,
+  });
+};
+
+export const VahvistettuViestiModal = ({
+  t,
+  theme,
+  open,
+  viestiMetadata,
+  handleClose,
+  handlePoistaViesti,
+  handleLisaaEditoriin,
+}: VahvistettuViestiModalProps) => {
+  const { viesti, isLoading, error } = useVahvistettuViesti(viestiMetadata?.id);
+  const { addToast } = useToaster();
+
+  useEffect(() => {
+    handleFetchError(addToast, error, 'virhe.viestinLataus', t);
+  }, [addToast, error, t]);
+
+  if (error) {
+    return null;
+  }
+
+  const content = isLoading ? (
+    <FullSpinner />
+  ) : (
+    <Stack gap={theme.spacing(2)}>
+      <OphTypography data-testid="viesti-otsikko" variant={'h2'}>
+        {viesti?.otsikko}
+      </OphTypography>
+      <ViestiContent>
+        <div
+          id="viestiContent"
+          dangerouslySetInnerHTML={{ __html: viesti?.viesti || '' }}
+        />
+      </ViestiContent>
+    </Stack>
+  );
+
+  const tyyppi =
+    viestiMetadata && viestiMetadata.tyyppi
+      ? t(`hakemus.viesti.${viestiMetadata.tyyppi}`)
+      : '';
+
+  return (
+    <Modal
+      open={open}
+      onClose={handleClose}
+      data-testid="vahvistettu-viesti-modal"
+    >
+      <Box sx={style}>
+        <Stack sx={{ height: '100%' }} direction="column" gap={2}>
+          <Stack direction="row" gap={2} justifyContent="space-between">
+            <OphTypography variant="h2">{tyyppi}</OphTypography>
+            <Stack direction="row" gap={2}>
+              <OphButton
+                data-testid={`viesti-modal-kopioi-button`}
+                variant="outlined"
+                startIcon={<CopyAll />}
+                onClick={() => handleCopy(viesti?.viesti || '', t, addToast)}
+              >
+                {t(`hakemus.viesti.kopioi`)}
+              </OphButton>
+              <OphButton
+                data-testid={`viesti-modal-kopioi-editoriin-button`}
+                variant="outlined"
+                startIcon={<EditOutlined />}
+                onClick={() => {
+                  handleLisaaEditoriin(viesti?.viesti || '');
+                  addToast({
+                    key: 'hakemus.viesti.kopioituEditoriinToast',
+                    message: t('hakemus.viesti.kopioituEditoriinToast'),
+                    type: 'success',
+                    timeMs: 2500,
+                  });
+                }}
+              >
+                {t(`hakemus.viesti.kopioiEditoriin`)}
+              </OphButton>
+              <OphButton
+                data-testid={`viesti-modal-poista-button`}
+                variant="outlined"
+                startIcon={<DeleteOutlined />}
+                onClick={() => handlePoistaViesti(viesti!.id!)}
+              >
+                {t(`yleiset.poista`)}
+              </OphButton>
+              <OphButton
+                data-testid="viesti-modal-sulje-button"
+                onClick={handleClose}
+                endIcon={<CloseIcon />}
+              >
+                {t('yleiset.sulje')}
+              </OphButton>
+            </Stack>
+          </Stack>
+          <Divider />
+          {content}
+        </Stack>
+      </Box>
+    </Modal>
+  );
+};
--- tutu-frontend/src/hooks/useVahvistetutViestit.ts
+++ tutu-frontend/src/hooks/useVahvistetutViestit.ts
@@ -1,12 +1,13 @@
 import { useQuery, useQueryClient } from '@tanstack/react-query';
+import { useCallback, useMemo } from 'react';
 
 import { doApiFetch } from '@/src/lib/tutu-backend/api';
-import { VahvistettuViestiListItem } from '@/src/lib/types/viesti';
+import { VahvistettuViestiListItem, Viesti } from '@/src/lib/types/viesti';
 
 export const VAHVISTETUT_VIESTIT_SORT_KEY = 'tutu-viestit-sort';
 
 const getVahvistetutViestit = async (
-  hakemusOid: string,
+  hakemusOid?: string,
 ): Promise<VahvistettuViestiListItem[]> => {
   const viestiListaSortParam = localStorage.getItem(
     VAHVISTETUT_VIESTIT_SORT_KEY,
@@ -19,14 +20,17 @@ const getVahvistetutViestit = async (
   return await doApiFetch(url, undefined, 'no-store');
 };
 
-export const useVahvistetutViestit = (hakemusOid: string) => {
-  const queryKey = ['vahvistetutViestit', hakemusOid];
+export const useVahvistetutViestit = (hakemusOid?: string) => {
+  const queryKey = useMemo(
+    () => ['vahvistetutViestit', hakemusOid],
+    [hakemusOid],
+  );
 
   const queryClient = useQueryClient();
 
-  const refresh = () => {
+  const refresh = useCallback(() => {
     queryClient.invalidateQueries({ queryKey });
-  };
+  }, [queryClient, queryKey]);
 
   const query = useQuery({
     queryKey,
@@ -35,5 +39,30 @@ export const useVahvistetutViestit = (hakemusOid: string) => {
     throwOnError: false,
   });
 
-  return { ...query, viestiLista: query.data, refresh };
+  return {
+    ...query,
+    viestiLista: query.data,
+    refresh: refresh,
+    isLoading: query.isLoading,
+    error: query.error,
+  };
+};
+
+export const useVahvistettuViesti = (id?: string) => {
+  const queryKey = ['vahvistettuViesti', id];
+
+  const query = useQuery({
+    queryKey,
+    queryFn: async (): Promise<Viesti> =>
+      await doApiFetch(`viesti/${id}`, undefined, 'no-store'),
+    enabled: !!id,
+    throwOnError: false,
+  });
+
+  return {
+    ...query,
+    viesti: query.data,
+    isLoading: query.isLoading,
+    error: query.error,
+  };
 };
