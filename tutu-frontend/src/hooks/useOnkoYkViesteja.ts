2efa8d9137a5b79962a209f90ddaeab7b4bd6ae2
Otto Leppänen <16340878+leppaott@users.noreply.github.com>
2026-05-29 08:51:11 +0300

    Korjaa ykOnkoViesteja query paramit

--- tutu-frontend/src/hooks/useOnkoYkViesteja.ts
+++ tutu-frontend/src/hooks/useOnkoYkViesteja.ts
@@ -3,11 +3,7 @@ import { useQuery } from '@tanstack/react-query';
 import { doApiFetch } from '@/src/lib/tutu-backend/api';
 
 export const getOnkoYkViesteja = async (): Promise<boolean> => {
-  const localStorageSearchParams = localStorage.getItem('tutu-query-string');
-  const url = localStorageSearchParams
-    ? `ykViestiOnkoViesteja?${localStorageSearchParams}`
-    : `ykViestiOnkoViesteja`;
-
+  const url = `ykViestiOnkoViesteja`;
   return await doApiFetch(url, undefined, 'no-store');
 };
 
